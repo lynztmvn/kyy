e0b96ae289405a55d00ab421167568a51731f331
lynztmvn <128647536+lynztmvn@users.noreply.github.com>
2023-04-10 16:05:59 +0800

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[45;1;39m          ⇱ SC PREMIUM ⇲                        \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\033[0;35m ✘\033[0m For Debian 10 64 bit                          "
+echo -e "\033[0;35m ✘\033[0m For Ubuntu 18.04 & Ubuntu 20.04 64 bit        "
+echo -e "\033[0;35m ✘\033[0m For VPS with KVM and VMWare virtualization    "
+echo -e "\033[0;35m ✘\033[0m Build Up By LYNZ STORE                       "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[45;1;39m          ⇱ THANKS TO ⇲                         \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\033[0;35m ❀\033[0m Allah SWT                                     "
+echo -e "\033[0;35m ❀\033[0m LynzSTORE                                     "
+echo -e "\033[0;35m ❀\033[0m Buy: 081918484372                                        "
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[45;1;39m          ⇱ LYNZ-GARDEN ⇲                     \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
